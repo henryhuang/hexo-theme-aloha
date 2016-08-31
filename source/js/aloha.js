7220716da9b4bbc47f485311b66fef4cabd102d3
henryhuang <huangyijie1989@gmail.com>
2016-08-31 21:20:22 +0800

    deving

--- source/js/aloha.js
+++ source/js/aloha.js
@@ -1,3 +1,8 @@
 $(function () {
     $("#content").css("min-height", $(window).height() - 40 -2 - 30);
+    $('pre').addClass('prettyprint');
+    // prettyprint();
+    // hljs.initHighlightingOnLoad();
+
+    prettyPrint();
 })
