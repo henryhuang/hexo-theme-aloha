70a9212aee961d7e1bfd161eb1a1e82b08cfa00d
henryhuang <huangyijie1989@gmail.com>
2016-09-03 01:19:19 +0800

    add search function based Algolia engine

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 ## Elul
 
 A Hexo theme, use Semantic UI.
+
+---
+
+#### Important Develop log
+
+- 20160903
+
+    Add search function that base algolia search engine, but the Algolia logo has not be add to page.
+    
+    - How to use search function
+    
+        add flowing content to site's _config.yml
+        
+        ``` yml
+       algolia:
+         applicationID: 'YOUR APPLICATION ID'
+         apiKey: 'YOUR APP KEY'
+         indexName: 'YOUR INDEX's NAME'
+        ```
--- source/js/aloha.js
+++ source/js/aloha.js
@@ -5,4 +5,46 @@ $(function () {
     // hljs.initHighlightingOnLoad();
 
     prettyPrint();
+
+
+    // $('.ui.search')
+    //     .search({
+    //         type          : 'category',
+    //         minCharacters : 3,
+    //         apiSettings   : {
+    //             onResponse: function(githubResponse) {
+    //                 var
+    //                     response = {
+    //                         results : {}
+    //                     }
+    //                     ;
+    //                 // translate GitHub API response to work with search
+    //                 $.each(githubResponse.items, function(index, item) {
+    //                     var
+    //                         language   = item.language || 'Unknown',
+    //                         maxResults = 8
+    //                         ;
+    //                     if(index >= maxResults) {
+    //                         return false;
+    //                     }
+    //                     // create new language category
+    //                     if(response.results[language] === undefined) {
+    //                         response.results[language] = {
+    //                             name    : language,
+    //                             results : []
+    //                         };
+    //                     }
+    //                     // add result to category
+    //                     response.results[language].results.push({
+    //                         title       : item.name,
+    //                         description : item.description,
+    //                         url         : item.html_url
+    //                     });
+    //                 });
+    //                 return response;
+    //             },
+    //             url: '//api.github.com/search/repositories?q={query}'
+    //         }
+    //     })
+    // ;
 })
