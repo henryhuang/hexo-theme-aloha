cb4a26254d9540c6ca64d4d56dd47f7a34149aac
henryhuang <huangyijie1989@gmail.com>
2016-09-10 22:19:13 +0800

    end all of v0.1.0's features

--- source/js/aloha-events.js
+++ source/js/aloha-events.js
@@ -1,10 +1,10 @@
 $(function () {
     $("#content").css("min-height", $(window).height() - 40 -2 - 30);
     $('pre').addClass('prettyprint');
-    $.scrollUp({
-        scrollDistance: 100,
-        scrollText: ''
-    });
+    // $.scrollUp({
+    //     scrollDistance: 100,
+    //     scrollText: ''
+    // });
     $("#menu-icon").on("click", function () {
         $('#menu-sidebar')
             .sidebar('setting', 'transition', 'overlay')
