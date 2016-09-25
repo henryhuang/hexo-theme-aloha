ddd0e37690b8711ddb8f04570de903de847af47c
yijhuang <yijhuang@ebay.com>
2016-09-26 01:01:20 +0800

    adjusted code highlight

--- source/js/aloha-events.js
+++ source/js/aloha-events.js
@@ -1,10 +1,4 @@
 $(function () {
-    // $("#content").css("min-height", $("#menu-sidebar").height() - 41);
-    $('pre').addClass('prettyprint');
-    // $.scrollUp({
-    //     scrollDistance: 100,
-    //     scrollText: ''
-    // });
     $("#menu-icon").on("click", function () {
         $('#menu-sidebar')
             .sidebar('setting', 'transition', 'overlay')
