54c2383ae8157832254147a59e11e7518d6b3241
Darius-XT <90099469+Darius-XT@users.noreply.github.com>
2023-04-27 03:53:00 +0800

    Update toc.js

--- scripts/helper/toc.js
+++ scripts/helper/toc.js
@@ -1,71 +1,78 @@
 'use strict';
 
-var cheerio;
+let cheerio;
 
 function tocHelper(str) {
+  if (!cheerio) cheerio = require('cheerio');
 
-    if (!cheerio) cheerio = require('cheerio');
+  const $ = cheerio.load(str);
+  const headings = $('h1, h2, h3, h4, h5, h6');
 
-    var $ = cheerio.load(str);
-    var headings = $('h1, h2, h3, h4, h5, h6');
+  if (!headings.length) return '';
 
-    if (!headings.length) return '';
+  const listNumber = true;
+  let result = '<ul class="ui list">';
+  const lastNumber = [0, 0, 0, 0, 0, 0];
+  let firstLevel = 0;
+  let lastLevel = 0;
+  let i = 0;
 
-    var listNumber = false;
-    var result = '<ol class="ui ordered list">';
-    var lastNumber = [0, 0, 0, 0, 0, 0];
-    var firstLevel = 0;
-    var lastLevel = 0;
-    var i = 0;
+  const style = '<style type="text/css">' +
+    '.ui.list li > a > span:first-child {' +
+    '  display: inline-block;' +
+    '  width: 5em;' +
+    '}' +
+    '</style>';
+  $('head').append(style);
 
-    headings.each(function() {
-        var level = +this.name[1];
-        var id = $(this).attr('id');
-        var text = $(this).text();
+  headings.each(function() {
+    const level = +this.name[1];
+    const id = $(this).attr('id');
+    const text = $(this).text();
 
-        lastNumber[level - 1]++;
+    lastNumber[level - 1]++;
 
-        for (i = level; i <= 5; i++) {
-            lastNumber[i] = 0;
-        }
-
-        if (firstLevel) {
-            for (i = level; i < lastLevel; i++) {
-                result += '</li></ol>';
-            }
-
-            if (level > lastLevel) {
-                result += '<ol>';
-            } else {
-                result += '</li>';
-            }
-        } else {
-            firstLevel = level;
-        }
-
-        result += '<li>';
-        result += '<a href="#' + id + '">';
-
-        if (listNumber) {
-            result += '<span>';
-
-            for (i = firstLevel - 1; i < level; i++) {
-                result += lastNumber[i] + '.';
-            }
-
-            result += '</span> ';
-        }
-
-        result += '<span>' + text + '</span></a>';
-
-        lastLevel = level;
-    });
-
-    for (i = firstLevel - 1; i < lastLevel; i++) {
-        result += '</li></ol>';
+    for (i = level; i <= 5; i++) {
+      lastNumber[i] = 0;
     }
 
-    return result;
+    if (firstLevel) {
+      for (i = level; i < lastLevel; i++) {
+        result += '</li></ul>';
+      }
+
+      if (level > lastLevel) {
+        result += '<ul>';
+      } else {
+        result += '</li>';
+      }
+    } else {
+      firstLevel = level;
+    }
+
+    result += '<li>';
+    result += '<a href="#' + id + '">';
+
+    if (listNumber) {
+      result += '<span>';
+
+      for (i = firstLevel - 1; i < level; i++) {
+        result += lastNumber[i] + '.';
+      }
+
+      result += '&nbsp;&nbsp;&nbsp;&nbsp;</span> ';
+    }
+
+    result += '<span>' + text + '</span></a>';
+
+    lastLevel = level;
+  });
+
+  for (i = firstLevel - 1; i < lastLevel; i++) {
+    result += '</li></ul>';
+  }
+
+  return result;
 }
 
 module.exports = tocHelper;
